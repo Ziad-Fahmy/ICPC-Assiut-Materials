c6e60e5322e4cb811358c66f30af3a15f3caa616
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-06-25 01:10:11 +0300

    Create V. PUM.cpp

---
+++ V. PUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    int t=1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=3;j++)
+        {
+            cout<<t<<" ";
+            t++;
+        }
+        cout<<"PUM"<<endl;t++;
+    }
+}
