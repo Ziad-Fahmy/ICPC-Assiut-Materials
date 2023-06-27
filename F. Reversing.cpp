0eed10e2d13a4bdcc6c1dcbf7d2984b7bbc9b60f
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-06-27 05:08:10 +0300

    Create F. Reversing.cpp

---
+++ F. Reversing.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);      //it's like int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    reverse(arr.begin(), arr.end());
+
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+
+}
+
