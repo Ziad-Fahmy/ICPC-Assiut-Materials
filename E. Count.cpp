bece2d3bf76bdec2bf355d7b60b638a9ee12f8c4
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-06-29 05:22:08 +0300

    Create E. Count.cpp

---
+++ E. Count.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int sum=0;
+
+    for(char c: s)
+    {
+        sum+=c-'0';
+    }
+
+    cout<<sum;
+}
