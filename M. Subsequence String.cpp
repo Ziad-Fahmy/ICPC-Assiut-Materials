2c6f498df9c68dda82791b26fc3228894afe2117
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-07-01 02:22:41 +0300

    Update M. Subsequence String.cpp

--- M. Subsequence String.cpp
+++ M. Subsequence String.cpp
@@ -29,3 +29,32 @@ int main() {
 
     return 0;
 }
+
+
+/*
+another way
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s,x="hello";
+    cin>>s;
+
+
+    int c=0;
+
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]==x[c]) c++;
+    }
+
+    if(c==5)
+    {
+        cout<<"YES"<<endl;
+    }
+    else cout<<"NO"<<endl;
+}
+
+*/
