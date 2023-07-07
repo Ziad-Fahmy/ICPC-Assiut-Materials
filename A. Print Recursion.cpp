b472396af76d36d8003aa9811fe3211145040b50
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-07-08 02:43:59 +0300

    Update A. Print Recursion.cpp

--- A. Print Recursion.cpp
+++ A. Print Recursion.cpp
@@ -1,3 +1,33 @@
+
+// recursion meen that we make a method like for loop and use it more than one time
+
+
+
+#include<iostream>
+using namespace std;
+
+void print(int n)
+{
+    if(n==0) return;
+    cout<<"I love Recursion"<<endl;
+
+    print(n-1);
+}
+
+int main()
+{
+    int n;cin>>n;
+    print(n);
+}
+
+
+
+
+
+/*
+
+<<this is direct way(we need recursion)>>
+
 #include<iostream>
 using namespace std;
 int main()
@@ -6,3 +36,4 @@ int main()
 
     for(int i=0;i<n;i++) cout<<"I love Recursion"<<endl;
 }
+*/
