522e1a3f3eeefa001b04cc6fcb1dd6b190268836
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-07-08 02:52:28 +0300

    Update B. Print from 1 to N.cpp

--- B. Print from 1 to N.cpp
+++ B. Print from 1 to N.cpp
@@ -1,9 +1,23 @@
 
 #include<iostream>
 using namespace std;
+
+void solve(int n)
+{
+
+    if(n==1) 
+    {
+        cout<<n<<endl;
+        return;
+    }
+    
+    solve(n-1);
+    cout<<n<<endl;
+}
+
+
 int main()
 {
     int n;cin>>n;
-
-    for(int i=1;i<=n;i++) cout<<i<<endl;
+    solve(n);
 }
